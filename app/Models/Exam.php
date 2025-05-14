425f06b6707593af00439bccc9e56a849534bd1b
Cisco Ramon <aungkoman@gmail.com>
2025-05-14 22:43:10 +0630

    exam and mark added

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Exam extends Model
 {
     use HasFactory;
+
+
+
+    protected $fillable = [
+        'name',
+        'date',
+        'subject_id',
+    ];
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function marks()
+    {
+        return $this->hasMany(Mark::class);
+    }
 }
