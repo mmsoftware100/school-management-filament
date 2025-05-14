ce27926734c7dad2fce0e3604edf61fac13e6c9d
Cisco Ramon <aungkoman@gmail.com>
2025-05-14 22:22:39 +0630

    Update Subject.php

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -4,8 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subject extends Model
 {
     use HasFactory;
+
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+
+    /**
+     * Exams related to this subject.
+     */
+    public function exams(): HasMany
+    {
+        return $this->hasMany(Exam::class);
+    }
+
+    /**
+     * Marks relationship 
+     */
+    public function timeTables(): HasMany
+    {
+        return $this->hasMany(TimeTable::class, 'subject_id');
+    }
 }
