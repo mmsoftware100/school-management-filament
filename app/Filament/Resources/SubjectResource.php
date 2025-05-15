0602e3020cece5f196697c1f6202b99afde92c94
Cisco Ramon <aungkoman@gmail.com>
2025-05-15 14:33:10 +0630

    subject resource is added

--- app/Filament/Resources/SubjectResource.php
+++ app/Filament/Resources/SubjectResource.php
@@ -10,6 +10,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -34,7 +35,7 @@ class SubjectResource extends Resource
     {
         return $table
             ->columns([
-                
+                TextColumn::make('serial_no')->label('No.')->rowIndex(),
                 TextColumn::make('name')->searchable()->sortable(),
             ])
             ->filters([
